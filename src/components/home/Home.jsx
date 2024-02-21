cdf3f43384e2d6ea2e06f7cf319c6b5153538c8e
Yash-946 <yashagrawal946@gmail.com>
2024-02-21 19:55:54 +0530

    home todo btn update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./Home.css"
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+  const history = useNavigate();
   return (
     <div className='home d-flex justify-content-center align-items-center'>
         <div className="container d-flex justify-content-center align-items-center flex-column">
             <h1 className='text-center'>Organize your <br /> work and life, finally</h1>
             <p>Become focused, organised, and calm with <br /> todo app. The World's #1 manger app.</p>
-            <button className='home-btn p-2'>Make Todo List</button>
+            <button className='home-btn p-2' onClick={() => history("/todo")}>Make Todo List</button>
         </div>
        
     </div>
